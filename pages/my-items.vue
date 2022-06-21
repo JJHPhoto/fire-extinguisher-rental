e6d44c6455f0f421c68f885355e324af6bd57b10
Joshua J. Haller <phone@joshuajhaller.com>
2022-06-21 18:46:24 +1000

    Created the Calendar modal and modified the state to add products to the my-items page when they are rented.

--- pages/my-items.vue
+++ pages/my-items.vue
@@ -1,13 +1,24 @@
 <template>
   <div>
-    My items. 
+    <MyItem 
+      v-for="item in myRentals"
+      :key="item.id"
+      :item="item"
+    />
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+import MyItem from '../components/MyItem.vue';
+
   export default {
-    layout: 'no-nav'
-  }
+    layout: "no-nav",
+    computed: {
+        ...mapState(["myRentals"])
+    },
+    components: { MyItem }
+}
 </script>
 
 <style  scoped>
